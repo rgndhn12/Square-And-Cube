734f0d56696260291ca0aad7b037c0779409d8ab
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-04-24 16:16:34 +0800

    Opening the integers.txt, double.txt, triple.txt!

--- SquareAndCube.py
+++ SquareAndCube.py
@@ -1,6 +1,8 @@
 #Dahan, Regine Fae M. BSCPE 1-5 File Handling #4
 
 #open integers.txt,create double.txt and triple.txt
+with open("integers.txt", "r") as integers_file, open("double.txt", "a") as squared_file, open("triple.txt", "a") as cubed_file:
+
 #read integers.txt
 
 #if it is even
