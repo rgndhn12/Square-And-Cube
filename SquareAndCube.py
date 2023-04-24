a847374bc9fc05b71d458844c3ece34c14a74921
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-04-24 16:07:26 +0800

    Pseudocode

---
+++ SquareAndCube.py
@@ -0,0 +1,14 @@
+#Dahan, Regine Fae M. BSCPE 1-5 File Handling #4
+
+#open integers.txt,create double.txt and triple.txt
+#read integers.txt
+
+#if it is even
+#   find the square of it
+#   write it in double.txt
+
+#if it is odd
+#   find the cube of it
+#   write it in triple.txt
+
+#display the output
